9549a339841246ba83ac620199acaa69e588a274
khomtali <42213097+khomtali@users.noreply.github.com>
2018-11-15 20:19:55 +0100

    Update authorization.php

--- view/authorization.php
+++ view/authorization.php
@@ -1,22 +1,6 @@
 <?php
-  require_once './model/authFunctions.php';
-
-  $errors = [];
-  if(!empty($_POST['login']) && !empty($_POST['password'])) {
-    if(!empty($_POST['signIn'])) {
-      if(login($_POST['login'], $_POST['password'])) {
-        header('Location: ./index.php');
-        die;
-      } else $errors[] = 'Incorrect login or password';
-    }
-    if(!empty($_POST['signUp'])) {
-      if(empty(userExistence($_POST['login']))) {
-        if(register($_POST['login'], $_POST['password'])) {
-          $errors[] = 'Registration is successful. Please, enter your personal data to sign in';
-        }
-      } else $errors[] = 'Login already exists. Choose another one';
-    }
-  } else $errors[] = 'Please, enter your personal data to sign in. Or sign up';
+  require_once '/var/www/user_data/ngubanova/me/join-mvc/model/authFunctions.php';
+  require_once '/var/www/user_data/ngubanova/me/join-mvc/controller/userController.php';
 ?>
 
 <html lang="en">

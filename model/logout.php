d44b4f7f14cb42eb33f77c8e4817340100f2b5a1
khomtali <42213097+khomtali@users.noreply.github.com>
2018-11-15 20:19:03 +0100

    Update logout.php

--- model/logout.php
+++ model/logout.php
@@ -1,6 +1,7 @@
 <?php
-    require_once 'authFunctions.php';
-    if(isAuthorized())
-        logOut();
-    header('Location: ./authorization.php');
+  require_once '/var/www/user_data/ngubanova/me/join-mvc/model/authFunctions.php';
+
+  if(isAuthorized())
+    logOut();
+  header('Location: /var/www/user_data/ngubanova/me/join-mvc/view/authorization.php');
 ?>
